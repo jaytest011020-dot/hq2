5bf945e21258dd812e8bc4f68c7af67289e55b37
jaylordlapena12-rgb <jaylordlapena12@gmail.com>
2025-09-12 09:06:46 +0800

    Update antibd.js

--- Rudra/events/antibd.js
+++ Rudra/events/antibd.js
@@ -1,20 +1,39 @@
 module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
-  version: "0.0.1",
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
-  description: "Against changing Bot's nickname"
+  version: "0.0.2",
+  credits: "Priyansh Rajput (modified by ChatGPT)",
+  description: "Prevents changing the bot's nickname"
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-    var { logMessageData, threadID, author } = event;
-    var botID = api.getCurrentUserID();
-    var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : BOTNAME;
-    if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
-        var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
-    }  
-        }
+  const { logMessageData, threadID, author } = event;
+  const botID = api.getCurrentUserID();
+  const { BOTNAME, ADMINBOT } = global.config;
+
+  // Get saved nickname from thread data, fallback to BOTNAME
+  let { nickname } = await Threads.getData(threadID, botID);
+  nickname = nickname ? nickname : BOTNAME;
+
+  // Check if someone (not bot or admin) changed bot nickname
+  if (
+    logMessageData.participant_id == botID &&
+    author != botID &&
+    !ADMINBOT.includes(author) &&
+    logMessageData.nickname != nickname
+  ) {
+    // Restore nickname
+    api.changeNickname(nickname, threadID, botID);
+
+    // Get user info
+    const info = await Users.getData(author);
+
+    // Custom message
+    return api.sendMessage(
+      {
+        body: `⚠️ Hey ${info.name}, you are not allowed to change the bot's nickname! 😼\nNickname has been restored to: ${nickname}`
+      },
+      threadID
+    );
+  }
+};
