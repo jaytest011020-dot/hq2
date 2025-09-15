9b84cdb0af92ad559db2ef35386c89e4b5bbda73
jaylordlapena20-rgb <jaylordlapena20@gmail.com>
2025-09-15 18:10:18 +0800

    Update kiss.js

--- Rudra/commands/kiss.js
+++ Rudra/commands/kiss.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "Kiss the person you want",
-    commandCategory: "Love",
+    commandCategory: "fun",
     usages: "kiss [tag]",
     cooldowns: 5,
     dependencies: {
