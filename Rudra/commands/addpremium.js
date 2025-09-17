1de80453eae94c7b0896144cb653e826ee85c33f
jaylordlapena21-afk <jaylordlapena21@gmail.com>
2025-09-18 07:30:41 +0800

    Update addpremium.js

--- Rudra/commands/addpremium.js
+++ Rudra/commands/addpremium.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const config = require("../../config.json");
+const config = require("../config.json"); // Correct path to config.json from 'commands' folder
 
 module.exports.config = {
   name: "addpremium",
