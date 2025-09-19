1a263b37ae439aefbb5a96212f34096c47eefb44
jaylordlapena21-afk <jaylordlapena21@gmail.com>
2025-09-19 15:45:55 +0800

    Update uid.js

--- Rudra/commands/uid.js
+++ Rudra/commands/uid.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Get User ID.",
-	commandCategory: "Tools",
+	commandCategory: "utility",
 	cooldowns: 5
 };
 
